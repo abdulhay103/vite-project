2ee31527158e023de2c651c2934b6108401712e6
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-05 14:39:18 +0600

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,16 @@
 
 const Hero = () => {
-    let marks = 55;
+    let cities = ["Dhaka", "Landon", "Ohio", "Shikago", "Korachi"];
     return (
         <div>
-            <h1>Immedietly invoke funtion:</h1>
-            {(()=>{
-                if(marks>80 && marks<100){
-                    return <h2>Congratulation! You Got A+</h2>
+            <h1>loop In jsx:</h1>
+            <ol>
+                {
+                    cities.map((item, index)=>{
+                        return <li key={index.toString()}>{item}</li>
+                    })
                 }
-                else if(marks>70 && marks<80){
-                        return <h2>Congratulation! You Got A</h2>
-                }
-                else if(marks>60 && marks<70){
-                        return <h2>Congratulation! You Got A-</h2>
-                }
-                else if(marks>50 && marks<60){
-                        return <h2>Congratulation! You Got B</h2>
-                }
-                else if(marks>40 && marks<50){
-                        return <h2>Congratulation! You Got C</h2>
-                }
-                else if(marks>33 && marks<40){
-                        return <h2>Congratulation! You Got D</h2>
-                }
-                else{
-                    return <h2>You fail the Exam</h2>
-                }
-            })()}
+            </ol>
         </div>
     );
 };
