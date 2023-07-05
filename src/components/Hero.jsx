2c62cac653cb46f263ceb9547b1f44bdba4e923a
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-05 13:49:26 +0600

    Jsx understanding done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ const Hero = () => {
     return (
         <div>
             <h1>Hello form Hero section!</h1>
+            <button style={{marginBottom:"15px"}} onClick={()=>{alert("hello")}} type="submit">Check Alert</button>
         </div>
     );
 };
