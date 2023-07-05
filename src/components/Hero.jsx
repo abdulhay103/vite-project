90c656ff9643851a24a11750173d92127ba7fe0d
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-05 14:28:35 +0600

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,32 @@
 
 const Hero = () => {
-    let marks = 95;
+    let marks = 55;
     return (
         <div>
-            <h1>Marks Checker</h1>
-            {marks>80?<h2>Briliant Result!</h2>:<h2>Avarage Result.</h2>}
+            <h1>Immedietly invoke funtion:</h1>
+            {(()=>{
+                if(marks>80 && marks<100){
+                    return <h2>Congratulation! You Got A+</h2>
+                }
+                else if(marks>70 && marks<80){
+                        return <h2>Congratulation! You Got A</h2>
+                }
+                else if(marks>60 && marks<70){
+                        return <h2>Congratulation! You Got A-</h2>
+                }
+                else if(marks>50 && marks<60){
+                        return <h2>Congratulation! You Got B</h2>
+                }
+                else if(marks>40 && marks<50){
+                        return <h2>Congratulation! You Got C</h2>
+                }
+                else if(marks>33 && marks<40){
+                        return <h2>Congratulation! You Got D</h2>
+                }
+                else{
+                    return <h2>You fail the Exam</h2>
+                }
+            })()}
         </div>
     );
 };
