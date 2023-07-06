8f786f127868bf539fa159161e57095d7595538b
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-06 09:52:14 +0600

    condition checked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@
 const Header = () => {
     return (
         <div>
-            <ul className="nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Contact</a></li>
+            <ul className="flex p-3">
+                <li className="px-2 text-lg font-bold text-orange-600"><a href="#">Home</a></li>
+                <li className="px-2 text-lg font-bold text-orange-600"><a href="#">Contact</a></li>
             </ul>
         </div>
     );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
-
+// let cities = ["Dhaka", "Landon", "Ohio", "Shikago", "Korachi"];
+const logInStatusBtn = (status)=>{
+    if(status){
+        return <button className="px-5 py-2 bg-red-500 text-white text-lg font-semibold cursor-pointer rounded">log Out</button>
+    }else{
+        return <button className="px-5 py-2 bg-green-500 text-white text-lg font-semibold cursor-pointer rounded">log In</button>
+    }
+}
 const Hero = () => {
-    let cities = ["Dhaka", "Landon", "Ohio", "Shikago", "Korachi"];
     return (
-        <div>
-            <h1>loop In jsx:</h1>
-            <ol>
-                {
-                    cities.map((item, index)=>{
-                        return <li key={index.toString()}>{item}</li>
-                    })
-                }
-            </ol>
+        <div className="px-10">
+            <h1 className='text-xl font-bold text-red-700 text-center'>loop In jsx:</h1>
+            { logInStatusBtn(true) }
         </div>
     );
 };
