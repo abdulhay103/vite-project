14e45c107ea8a357a532904abd9f397598323ed3
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-07 23:50:12 +0600

    invoked function checked

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,21 @@
 // let cities = ["Dhaka", "Landon", "Ohio", "Shikago", "Korachi"];
 const Hero = () => {
-    let status = true;
-    let text = 51;
+    let status = false;
+    let text = 2;
     return (
         <div className="px-10">
             <h1 className='text-xl font-bold text-red-700 text-center'>Conditional operator && In jsx:</h1>
-            {
-                status && text >=5 &&
-                <button onClick={()=>{setTimeout(() => {
-                    alert("you are logined. Are you sure to log out")
-                }, 2000);}} className="px-5 py-2 bg-red-500 text-white text-lg font-semibold cursor-pointer rounded">log Out</button>
-            }
+            {(()=>{
+                if(status && text >=5){
+                    return <button onClick={()=>{setTimeout(() => {
+                        alert("you are logined. Are you sure to log out")
+                    }, 1000);}} className="px-5 py-2 bg-red-500 text-white text-lg font-semibold cursor-pointer rounded">log Out</button>
+                }else{
+                    return <button onClick={()=>{setTimeout(() => {
+                        alert("you are not logined. Make sure your login status")
+                    }, 1000);}} className="px-5 py-2 bg-green-500 text-white text-lg font-semibold cursor-pointer rounded">log In</button>
+                }
+            })()}
         </div>
     );
 }
