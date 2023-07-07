db3fa30f4dd6deef471f181ea92a13da999d0c19
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-08 01:03:53 +0600

    succesfully passing props

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,10 @@
+import HeroText from "./HeroText";
 // let cities = ["Dhaka", "Landon", "Ohio", "Shikago", "Korachi"];
 const Hero = () => {
-    let status = false;
-    let text = 2;
     return (
         <div className="px-10">
-            <h1 className='text-xl font-bold text-red-700 text-center'>Conditional operator && In jsx:</h1>
-            {(()=>{
-                if(status && text >=5){
-                    return <button onClick={()=>{setTimeout(() => {
-                        alert("you are logined. Are you sure to log out")
-                    }, 1000);}} className="px-5 py-2 bg-red-500 text-white text-lg font-semibold cursor-pointer rounded">log Out</button>
-                }else{
-                    return <button onClick={()=>{setTimeout(() => {
-                        alert("you are not logined. Make sure your login status")
-                    }, 1000);}} className="px-5 py-2 bg-green-500 text-white text-lg font-semibold cursor-pointer rounded">log In</button>
-                }
-            })()}
+            <h1 className='text-xl font-bold text-red-700 text-center'>Passing props</h1>
+            <HeroText firstProps="this is my first props"/>
         </div>
     );
 }
---
+++ src/components/HeroText.jsx
@@ -0,0 +1,11 @@
+
+const HeroText = (props) => {
+
+    return (
+        <div>
+           <h1>{props.firstProps}</h1>
+        </div>
+    );
+};
+
+export default HeroText;
