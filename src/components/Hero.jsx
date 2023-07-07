e8424eab57e3adeca768e285c362e87b79153905
abdulhay103 <abdulhay.me@gmail.com>
2023-07-07 15:23:37 +0600

    set time out done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 // let cities = ["Dhaka", "Landon", "Ohio", "Shikago", "Korachi"];
 const logInStatusBtn = (status)=>{
     if(status){
-        return <button className="px-5 py-2 bg-red-500 text-white text-lg font-semibold cursor-pointer rounded">log Out</button>
+        return <button onClick={()=>{setTimeout(() => {
+            alert("you are logined. Pls explore new things.")
+        }, 2000);}} className="px-5 py-2 bg-red-500 text-white text-lg font-semibold cursor-pointer rounded">log Out</button>
     }else{
-        return <button className="px-5 py-2 bg-green-500 text-white text-lg font-semibold cursor-pointer rounded">log In</button>
+        return <button onClick={()=>{setTimeout(() => {
+            alert("you are logouted! Pls log in again.")
+        }, 2000);}} className="px-5 py-2 bg-green-500 text-white text-lg font-semibold cursor-pointer rounded">log In</button>
     }
 }
 const Hero = () => {
