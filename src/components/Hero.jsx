71f2f1880bbe4af42e5226e49f052f04d3fa7bd3
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-09 02:11:38 +0600

    event handled with state

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,10 @@
 import HeroText from "./HeroText";
-import cities from "../assets/datas/citiesArray"
 const Hero = () => {
-
-    function cityReader() {
-        if(cities){
-            return cities.map((city, index)=>{
-                return <li key={index}>{city} and it key value is: {index}</li>
-            })
-        }
-    }
 
     return (
         <div className="px-10">
             <h1 className='text-xl font-bold text-red-700 text-center'>Passing Object by props</h1>
-            <HeroText myFun={cityReader}/>
+            <HeroText />
         </div>
     );
 }
--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,10 +1,27 @@
-const HeroText = ({myFun}) => {
+import cities from "../assets/datas/citiesArray";
+import React, { useState } from "react";
+
+const HeroText = () => {
+    const [showCities, setShowCities] = useState(false);
+
+    const citiShower = () => {
+        if (cities) {
+            return cities.map((city, index) => {
+                return <li key={index}>{city}</li>;
+            });
+        }
+    };
+
+    const handleClick = () => {
+        setShowCities(true);
+    };
 
     return (
         <div>
-            <ul>
-                {myFun()}
-            </ul>
+            <button onClick={handleClick} className="px-3 py-2 bg-slate-700 rounded-md text-white">
+                Click To Show Cities
+            </button>
+            {showCities && <ul>{citiShower()}</ul>}
         </div>
     );
 };
